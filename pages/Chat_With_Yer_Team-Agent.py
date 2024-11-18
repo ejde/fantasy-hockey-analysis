b87ddc0fbafe84205c0ea8a1ad0a4a8a776af744
ejde <ejdevangelista@gmail.com>
2024-11-17 19:31:37 -0800

    More tests with agent tooling

--- pages/Chat_With_Yer_Team-Agent.py
+++ pages/Chat_With_Yer_Team-Agent.py
@@ -8,10 +8,23 @@ from langchain.agents import create_structured_chat_agent
 from langchain_core.tools import StructuredTool
 from langchain import hub
 from langchain_community.tools import TavilySearchResults
-
+from langchain.agents import AgentExecutor
 
 st.title("💬 Chat With Yer Team - Agent Style")
 st.text("This is a test")
+
+if not st.session_state.get('logged_in'):
+    st.info("Please log in using the sidebar on the main page to proceed.")
+    st.page_link("Home.py", label="Go Home")
+    st.stop()
+
+if 'selected_team_name' not in st.session_state or 'roster' not in st.session_state:
+    st.info("Please wait for the login process on the Home page to complete")
+    st.stop()
+
+# Initialize Streamlit session state for messages
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
 
 os.environ['TAVILY_API_KEY'] = st.secrets.get('tavily_key')
 
@@ -21,20 +34,6 @@ os.environ['LANGCHAIN_ENDPOINT'] = "https://api.smith.langchain.com"
 os.environ['LANGCHAIN_API_KEY'] = st.secrets.get('langsmith_key')
 os.environ['LANGCHAIN_PROJECT'] = st.secrets.get('langsmith_project')
 
-
-if not st.session_state.get('logged_in'):
-    st.info("Please log in using the sidebar on the main page to proceed.") 
-    st.page_link("Home.py", label="Go Home")
-    st.stop()
-
-if 'selected_team_name' not in st.session_state and 'roster' not in st.session_state:
-    st.info("Please wait for the login process on the Home page to complete") 
-    st.stop()
-
-# Initialize Streamlit session state for messages
-if 'messages' not in st.session_state:
-    st.session_state.messages = []
-    
 api = FantraxAPI(st.session_state['league_id'], session=st.session_state['session'])
 standings_df = utils.fetch_standings(api)
 roster_df = utils.fetch_team_roster(api, st.session_state['selected_team_id'])
@@ -43,17 +42,16 @@ input_data = {
     "standings": json.loads(standings_df.to_json(orient="records"))
 }
 
-def featch_league_standings():
+def fetch_league_standings():
     return utils.fetch_standings(api)
 
 def fetch_user_team_roster():
     return utils.fetch_team_roster(api, st.session_state['selected_team_id'])
-    
+
 def fetch_opposing_team_roster(team_name):
     tl = [team for team in api.teams if team.name == team_name]
     if not tl:
         return None
-
     return utils.fetch_team_roster(api, tl[0])
 
 def fetch_user_team_name():
@@ -72,32 +70,25 @@ def search_game_scores(query: str):
 
 tools = [
     StructuredTool.from_function(search_player_news, name="Search Player Info", description="Search for recent player information and performance"),
-    #StructuredTool.from_function(utils.search_roster_player_news, name="Search Roster Player News", description="Search for recent roster player performance and analysis"),
-    #StructuredTool.from_function(utils.fetch_roster_news, name="Fetch Roster News", description="Fetch latest news for each player on the roster of the user's team"),
-
-    StructuredTool.from_function(featch_league_standings, name="Fetch League Standings", description="Fetch the league standings"),    
-
+    StructuredTool.from_function(fetch_league_standings, name="Fetch League Standings", description="Fetch the league standings"),
     StructuredTool.from_function(fetch_user_team_roster, name="Fetch User's Team Roster", description="Get the roster of the user's team"),
     StructuredTool.from_function(fetch_user_team_name, name="Fetch User's Team Name", description="Get the name of the user's team"),
-
-    StructuredTool.from_function(fetch_current_free_agents, name="Fetch Free Agents", description="Get a list of top available free agents of a given position, 'F', 'D' or 'G'"),
+    StructuredTool.from_function(fetch_current_free_agents, name="Fetch Free Agents", description="Get a list of top available free agents of a given position, pass a string whose value is one of 'F', 'D' or 'G'"),
     StructuredTool.from_function(fetch_opposing_team_roster, name="Fetch Opposing Team Roster", description="Get the roster of an opposing team for potential trades"),
-
     StructuredTool.from_function(search_game_scores, name="Search Game Scores", description="Search for recent game scores of a given team")
 ]
 
 if st.session_state['league_id'] in st.secrets.get("league_whitelist", []):
     llm_api_key = st.secrets.get("openai_key")
 else:
-    llm_api_key = st.text_input("Enter your OpenAI API key:", type="password")     
+    llm_api_key = st.text_input("Enter your OpenAI API key:", type="password")
 
-llm = llm = ChatOpenAI(openai_api_key=llm_api_key, model_name="gpt-4")
+llm = ChatOpenAI(openai_api_key=llm_api_key, model_name="gpt-4o-mini")
 chat_prompt = hub.pull("danglesnipecelly/fantasy-hockey-coach")
 agent = create_structured_chat_agent(llm=llm, tools=tools, prompt=chat_prompt)
 
 # Wrap the agent in an AgentExecutor to manage interaction flow
-from langchain.agents import AgentExecutor
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, return_intermediate_steps=False)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, return_intermediate_steps=False, handle_parsing_errors=True)
 
 with st.sidebar:
     if st.button("Clear Chat Window", use_container_width=True, type="primary"):
@@ -105,7 +96,7 @@ with st.sidebar:
         st.rerun()
 
 for message in st.session_state.messages:
-     if message['role'] in ['user', 'ai']:
+    if message['role'] in ['user', 'ai']:
         with st.chat_message(message['role']):
             st.markdown(message['content'])
 
@@ -118,10 +109,14 @@ if prompt := st.chat_input("Are you ready? Good, cuz yer goin!"):
         message_placeholder.markdown("S'YeahSo...")
         response = ''
         try:
-            response = agent_executor.invoke({"input": prompt, "chat_history": st.session_state.messages})
-            message_placeholder.markdown(response['output'])
+            response = agent_executor({"input": prompt, "chat_history": st.session_state.messages})
+            if isinstance(response, dict) and 'output' in response:
+                message_placeholder.markdown(response['output'])
+                st.session_state.messages.append({'role': 'ai', 'content': response['output']})
+            else:
+                message_placeholder.markdown(response)
+                st.session_state.messages.append({'role': 'ai', 'content': response})
         except Exception as e:
             st.exception(e)
         
         st.session_state.messages.append({'role': 'user', 'content': prompt})
-        st.session_state.messages.append({'role': 'ai', 'content': response})
